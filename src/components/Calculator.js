d1e5c73279befdf409ab3422803f58b5ef5bb919
trance <michaelmunavu83@gmail.com>
2022-07-07 12:39:40 +0300

    Corrected linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,11 @@
-import React , {useState } from "react";
-import calculate from "../logic/calculate";
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
   const [calc, setCalc] = useState({
     total: 0,
-    next: "",
-    operation: "",
+    next: '',
+    operation: '',
   });
 
   const clickCalc = (e) => {
@@ -15,10 +15,10 @@ const Calculator = () => {
 
   const results = `
     ${
-      calc.total || calc.operation || calc.next
-        ? `${calc.total || ""} ${calc.operation || ""} ${calc.next || ""}`
-        : 0
-    }`;
+  calc.total || calc.operation || calc.next
+    ? `${calc.total || ''} ${calc.operation || ''} ${calc.next || ''}`
+    : 0
+}`;
 
   return (
     <div className="calcApp">
@@ -27,35 +27,35 @@ const Calculator = () => {
         <div className="current">{results}</div>
       </div>
 
-      <button type ="button" onClick ={clickCalc} >AC</button>
-      <button type ="button" onClick ={clickCalc}>+/-</button>
-      <button type ="button" onClick ={clickCalc} >%</button>
-      <button type ="button" onClick ={clickCalc} className="operand" >
+      <button type="button" onClick={clickCalc}>AC</button>
+      <button type="button" onClick={clickCalc}>+/-</button>
+      <button type="button" onClick={clickCalc}>%</button>
+      <button type="button" onClick={clickCalc} className="operand">
         ÷
       </button>
-      <button type ="button" onClick ={clickCalc} >7</button>
-      <button type ="button" onClick ={clickCalc} >8</button>
-      <button type ="button" onClick ={clickCalc} >9</button>
-      <button type ="button" onClick ={clickCalc}  className="operand">
+      <button type="button" onClick={clickCalc}>7</button>
+      <button type="button" onClick={clickCalc}>8</button>
+      <button type="button" onClick={clickCalc}>9</button>
+      <button type="button" onClick={clickCalc} className="operand">
         x
       </button>
-      <button type ="button" onClick ={clickCalc} >4</button>
-      <button type ="button" onClick ={clickCalc} >5</button>
-      <button type ="button" onClick ={clickCalc} >6</button>
-      <button type ="button" onClick ={clickCalc} className="operand" >
+      <button type="button" onClick={clickCalc}>4</button>
+      <button type="button" onClick={clickCalc}>5</button>
+      <button type="button" onClick={clickCalc}>6</button>
+      <button type="button" onClick={clickCalc} className="operand">
         -
       </button>
-      <button type ="button" onClick ={clickCalc} >1</button>
-      <button type ="button" onClick ={clickCalc} >2</button>
-      <button type ="button" onClick ={clickCalc} >3</button>
-      <button type ="button"  onClick ={clickCalc} className="operand">
+      <button type="button" onClick={clickCalc}>1</button>
+      <button type="button" onClick={clickCalc}>2</button>
+      <button type="button" onClick={clickCalc}>3</button>
+      <button type="button" onClick={clickCalc} className="operand">
         +
       </button>
-      <button  type ="button" onClick ={clickCalc} className="span-two">
+      <button type="button" onClick={clickCalc} className="span-two">
         0
       </button>
-      <button type ="button" onClick ={clickCalc} >.</button>
-      <button type ="button" onClick ={clickCalc} className="operand" >
+      <button type="button" onClick={clickCalc}>.</button>
+      <button type="button" onClick={clickCalc} className="operand">
         =
       </button>
     </div>
