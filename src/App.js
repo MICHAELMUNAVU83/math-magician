8dcba0b01b4b8c1cd1e923fd0dc25f449f1b8a51
trance <michaelmunavu83@gmail.com>
2022-07-11 06:45:38 +0300

    Aded react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Calculator from './components/Calculator';
 import Navbar from './components/Navbar';
 // import Quote from './components/Quote';
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
+import '../Calculator.css';
 
 const Calculator = () => {
   const [calc, setCalc] = useState({
