f0d2f13a0512d3511c4200702545c5e421eac118
trance <michaelmunavu83@gmail.com>
2022-07-11 06:23:24 +0300

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <h4>MATH MAGICIANS</h4>
+            <a href ="/">HOME</a>
+            <a href = "/calculator">CALCULATOR</a>
+            <a href ="/quote">QUOTE</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
