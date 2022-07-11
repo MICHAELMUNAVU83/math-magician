04e68e9ec9e1dcaec5f10c55a5b60e5c85f7dae5
trance <michaelmunavu83@gmail.com>
2022-07-11 06:21:10 +0300

    Add text to Quote.js

---
+++ src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Quote() {
+  return (
+    <div>
+      <h3>
+        Without mathematics, there’s nothing you can do. Everything around you
+        is mathematics. Everything around you is numbers. — Shakuntala Devi,
+        Indian writer and mental calculator
+      </h3>
+    </div>
+  );
+}
+
+export default Quote;
