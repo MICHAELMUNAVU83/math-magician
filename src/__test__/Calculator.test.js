23df42d4fd4dff8ff0db161ebfff604e31fc64de
trance <michaelmunavu83@gmail.com>
2022-07-14 07:08:47 +0300

    Added fireEvent to Calculator js to test interaction

--- src/__test__/Calculator.test.js
+++ src/__test__/Calculator.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Calculator from '../components/Calculator';
-import {render , cleanup} from "@testing-library/react"
+import {render , cleanup , fireEvent} from "@testing-library/react"
 afterEach(cleanup)
 
 it('Calculator renders correctly', () => {
   const{getByTestId} = render (<Calculator/>)
   expect(getByTestId("output")).toHaveTextContent('0')
- 
+  
 });
+it('shows user clicking on a button',()=>{
+  const{getByTestId , getByText} = render (<Calculator/>)
+  fireEvent.click(getByText('1'))
+  expect(getByTestId("output")).toHaveTextContent('1')
+
+
+})
+it('shows user clicking on a button',()=>{
+  const{getByTestId , getByText} = render (<Calculator/>)
+  fireEvent.click(getByText('AC'))
+  expect(getByTestId("output")).toHaveTextContent('0')
+
+
+})
